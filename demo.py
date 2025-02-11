713202c80505eb4f2209c010cb6b95040395c475
Psyguy <tanaka41@hotmail.com>
2025-02-10 19:34:52 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,21 @@
-print('hello')
-num_1 = int(input('Pick a number:'))
-num_2 = int(input('Pick another number:'))
+#Creating a better calculator than above
+num1 = float(input("Please choose a number.."))
+num2 = float(input("Please choose another number..."))
 
-add = num_1 + num_2
+#num1, num2 = 25, 30
+op = input("Please choose an operation! ")
+#operation = ['divide', 'multiply', 'add', 'subtract']
 
-print(add)
+if op == 'divide':
+  if num2 == 0:
+    print("Error. Division by zero")
+  else:
+      print(num1 / num2)
+elif op == 'add':
+    print(num1 + num2)
+elif op == 'subtract':
+    print(num1 - num2)
+elif op == 'multiply':
+    print(num1*num2)
+  
+
